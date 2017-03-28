bf7c71761b33503c916508e684cebf89307069f1
Zoltrix <haron.shihab88@gmail.com>
2017-03-28 12:16:59 +0200

    Add user token features per tweet, not per user

--- scripts/main.R
+++ scripts/main.R
@@ -1,5 +1,10 @@
 setwd("Projects/authorship-attribution/")
 
+
+## 1) Continue with investigating the distribution of the newly added features
+## we might need to scale them according to the algorithm
+
+## 2) Do not use aggregation ... use per document calculations instead
 # LIBRARIES ----
 library(readr)
 library(tidyr)
@@ -86,19 +91,19 @@ user_tokens <- users %>%
   unnest_tokens(word, text, token = stringr::str_split, pattern = " ") %>% 
   group_by(uname, file_id) %>%
   mutate(
-    new_lines = cumsum(str_count(word, "\\\\n")),
-    special_characters = cumsum(str_count(word, "[!\\:\\?]")),
-    underscores = cumsum(str_count(word, "_"))
+    new_lines = str_count(word, "\\\\n"),
+    special_characters = str_count(word, "[!\\:\\?]"),
+    underscores = str_count(word, "_")
     #TODO- emojis
     
   ) %>% ungroup()
 
 user_tokens.agg <- user_tokens %>% 
-  group_by(uname) %>% 
+  group_by(uname, file_id) %>% 
   summarise(
-    avg_newlines = mean(new_lines),
-    avg_special_chars = mean(special_characters),
-    avg_underscores = mean(underscores)
+    newlines = sum(new_lines),
+    special_chars = sum(special_characters),
+    underscores = sum(underscores)
   )
 
 users <- users %>% 

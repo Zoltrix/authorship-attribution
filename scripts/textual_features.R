4e05d327cb4cc95044363f8258c2a26078b7e5e8
Zoltrix <haron.shihab88@gmail.com>
2017-03-28 15:56:33 +0200

    fix the method of counting spaces

--- scripts/textual_features.R
+++ scripts/textual_features.R
@@ -1,12 +1,13 @@
 add_textual_features <- function(users) {
   user_tokens <- users %>% 
-    unnest_tokens(word, text, token = stringr::str_split, pattern = " ") %>% 
-    group_by(uname, file_id) %>%
+    unnest_tokens(word, text, token = stringr::str_split, pattern = " ", drop = FALSE) %>% 
+    group_by(uname, file_id, text) %>%
     mutate(
       new_lines = str_count(word, "\\\\n"),
       special_characters = str_count(word, "[!\\:\\?]"),
       underscores = str_count(word, "_"),
-      n_chars = nchar(word)
+      n_chars = nchar(word),
+      n_spaces = str_count(text, " ")
       #TODO- emojis
       
     ) %>% ungroup()
@@ -17,7 +18,8 @@ add_textual_features <- function(users) {
       newlines = sum(new_lines),
       special_chars = sum(special_characters),
       underscores = sum(underscores),
-      n_chars = sum(n_chars)
+      n_spaces = last(n_spaces), 
+      n_chars = sum(n_chars) + n_spaces
     )
   
   users %>% 

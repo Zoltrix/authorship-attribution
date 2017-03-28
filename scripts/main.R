96e122775a938330b572237224ebe358b8ee1052
Zoltrix <haron.shihab88@gmail.com>
2017-03-28 12:58:13 +0200

    Separate data processing logic from main.R

--- scripts/main.R
+++ scripts/main.R
@@ -1,6 +1,5 @@
 setwd("Projects/authorship-attribution/")
 
-
 ## 1) Continue with investigating the distribution of the newly added features
 ## we might need to scale them according to the algorithm
 
@@ -18,46 +17,14 @@ library(tm)
 
 # SOURCE FILES ----
 source("scripts/read_data.R")
+source("scripts/split_data.R")
+source("scripts/process.R")
 
 users <- read_data("data/sample/users/")
 
-train_model <- function(df, stop_words, method) {
-  corpus <- VCorpus(VectorSource(df$text))
-  
-  corpus <- tm_map(corpus, removeWords, stop_words) 
-  
-  tdm <- DocumentTermMatrix(corpus, control = list(removePunctuation = TRUE, 
-                                         #stopwords = TRUE, 
-                                         #stemming = TRUE, 
-                                         removeNumbers = TRUE, 
-                                         weighting = weightTfIdf,
-                                         bounds = list(global = c(5, Inf))))
-  
-  BigramTokenizer <-
-    function(x)
-      unlist(lapply(ngrams(words(x), 2), paste, collapse = " "), use.names = FALSE)
-  #library(RWeka)
-  #BigramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 2, max = 2))
-  
-  bigrams_tdm <- DocumentTermMatrix(corpus, control = list(tokenize = BigramTokenizer, 
-                                                   removeNumbers = TRUE, 
-                                                   weighting = weightTfIdf,
-                                                   bounds = list(global = c(2, Inf))))
-
-  train_words <- as.data.frame(as.matrix(tdm))
-  train_bigrams <- as.data.frame(as.matrix(bigrams_tdm))
-  
-  train <- bind_cols(train_words, train_bigrams)
-  train$y <- as.factor(df$y)
-  
-  train <- train %>% 
-    mutate(row_sums = rowSums( .[1: (ncol(train)-1)] ) ) %>% 
-    filter(row_sums > 0)
-  
-  registerDoMC(cores = 4)
-  
+train_model <- function(df, method) {
   train_control<- trainControl(method="cv", number=3, savePredictions = TRUE)
-  mdl <- train(y ~ ., data = train, trControl = train_control, method = method)
+  mdl <- train(y ~ ., data = df, trControl = train_control, method = method)
   
   mdl$pred
 }
@@ -84,9 +51,9 @@ user_tokens.agg <- user_tokens %>%
 users <- users %>% 
   inner_join(user_tokens.agg)
 
-users$y <- users$uname
-users$uname <- NULL
-
 stop_words <- read_lines("data/stopwords.txt")
 
-pred <- train_model(users, stop_words, 'svmLinear')
+registerDoMC(cores = 4)
+preprcess <- process_data(users, stop_words)
+
+pred <- train_model(preprcess$processed_df, 'svmLinear')

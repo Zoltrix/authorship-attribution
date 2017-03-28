258252cb6ecf5c954200ec887d331531435bb841
Zoltrix <haron.shihab88@gmail.com>
2017-03-28 15:14:59 +0200

    Remove specific seed for split_data; use global seed instead

--- scripts/split_data.R
+++ scripts/split_data.R
@@ -1,8 +1,7 @@
-split_data <- function(data, ratio = 0.7, seed = 1234) {
+split_data <- function(data, ratio = 0.7) {
   data <- data %>%
     mutate(id = row_number())
   
-  set.seed(seed)
   training <- data %>% 
     group_by(uname) %>% 
     sample_frac(ratio)
